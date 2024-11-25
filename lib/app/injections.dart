4c7c3c3eddbfbb7b38f6ffea8d39973fc077a274
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2024-11-25 11:56:28 -0300

    feat: history injections

--- lib/app/injections.dart
+++ lib/app/injections.dart
@@ -1,5 +1,8 @@
 import 'package:get_it/get_it.dart';
+import 'package:kondus/src/modules/history/history_injections.dart';
 
 final getIt = GetIt.instance;
 
-Future<void> initInjections() async {}
+Future<void> initInjections() async {
+  await historyInjections();
+}
