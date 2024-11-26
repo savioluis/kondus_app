74d1655a2abf8478afa5fb38014d494a7e8b0492
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2024-11-26 17:12:51 -0300

    feat: profile banner

--- lib/src/modules/profile/widgets/profile_banner.dart
+++ lib/src/modules/profile/widgets/profile_banner.dart
@@ -1,12 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:kondus/src/modules/profile/domain/profile_model.dart';
+import 'package:kondus/src/modules/profile/domain/profile_viewmodel.dart';
+import 'package:kondus/src/modules/shared/theme/app_theme.dart';
 
 class ProfileBanner extends StatelessWidget {
-  final ProfileModel model;
-  const ProfileBanner({super.key, required this.model});
+  final ValueNotifier<ProfileState> state;
+  const ProfileBanner({super.key, required this.state});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      constraints: const BoxConstraints(minHeight: 100),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: context.lightGreyColor.withOpacity(0.2),
+        borderRadius: BorderRadius.circular(18),
+      ),
+      child: ValueListenableBuilder(
+        valueListenable: state,
+        builder: (context, state, widget) => switch (state) {
+          ProfileIdleState() => const SizedBox(),
+          ProfileErrorState() => const SizedBox(),
+          ProfileLoadingState() => const Center(
+              child: CircularProgressIndicator(),
+            ),
+          ProfileSuccessState(model: final model) => Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    model.owner,
+                    style: context.headlineLarge,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                  ),
+                  Text(
+                    "Residência: ${model.address}",
+                    style: context.bodyLarge,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                  ),
+                  Text(
+                    model.complement, style: context.labelMedium,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                  )
+                ],
+              ),
+            ),
+        },
+      ),
+    );
   }
 }
