c82037cd287b792ce4f2e2dd4fd3e45906e04dd9
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2024-11-27 17:43:42 -0300

    feat: SettingsPage NotificationSection

---
+++ lib/src/modules/settings/widgets/notification_switch_row.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:kondus/src/modules/shared/theme/app_theme.dart';
+
+class NotificationSwitchRow extends StatelessWidget {
+  final String text;
+  final bool switchValue;
+  final Function() onTap;
+  const NotificationSwitchRow(
+      {super.key,
+        required this.text,
+        required this.switchValue,
+        required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: onTap,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            text,
+            style: context.titleMedium,
+          ),
+          Switch(
+            value: switchValue,
+            onChanged: (_) => onTap(),
+          ),
+        ],
+      ),
+    );
+  }
+}
