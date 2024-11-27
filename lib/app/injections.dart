2e7537e3d16249cf02a8e5394cdc612ac5be889b
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2024-11-27 14:31:17 -0300

    feat: ProductDetails injections

--- lib/app/injections.dart
+++ lib/app/injections.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:kondus/src/modules/history/history_injections.dart';
+import 'package:kondus/src/modules/product_details/product_details_injections.dart';
 import 'package:kondus/src/modules/profile/profile_injections.dart';
 
 final getIt = GetIt.instance;
@@ -7,4 +8,5 @@ final getIt = GetIt.instance;
 Future<void> initInjections() async {
   await historyInjections();
   await profileInjections();
+  await productDetailsInjections();
 }
