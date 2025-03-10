75753a79de823fb4f46283a112a6a304b0327822
savioluis <savioluislobo@gmail.com>
2025-03-10 18:11:47 -0300

    fix: fixed overflow and responsiveness issues in Login and Home AppBars

--- lib/src/modules/home/widgets/app_bar/home_app_bar.dart
+++ lib/src/modules/home/widgets/app_bar/home_app_bar.dart
@@ -5,7 +5,7 @@ import 'package:kondus/src/modules/shared/theme/app_theme.dart';
 class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
   const HomeAppBar({
     required this.username,
-    this.height = 86,
+    this.height = 96,
     super.key,
   });
 
@@ -14,21 +14,27 @@ class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 24, right: 24, top: 64),
-          child: Header(username: username),
-        ),
-        const SizedBox(height: 12),
-        Divider(
-          thickness: 0.1,
-          color: context.lightGreyColor,
-          height: 2,
-        ),
-      
-      ],
+    return SafeArea(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 24,
+              right: 24,
+              bottom: 6,
+            ),
+            child: Header(username: username),
+          ),
+          const SizedBox(height: 12),
+          Divider(
+            thickness: 0.1,
+            color: context.lightGreyColor,
+            height: 2,
+          ),
+        ],
+      ),
     );
   }
 
--- lib/src/modules/login/widgets/login_app_bar.dart
+++ lib/src/modules/login/widgets/login_app_bar.dart
@@ -15,42 +15,38 @@ class LoginAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return ClipPath(
       clipper: _LoginClipPath(),
-      child: AspectRatio(
-        aspectRatio: 2,
-        child: Container(
-          decoration: BoxDecoration(
-            color: context.blueColor,
-          ),
-          padding: const EdgeInsets.only(top: 72, left: 24),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                title,
-                style: TextStyle(
-                  color: context.whiteColor,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 36,
-                ),
+      child: Container(
+        decoration: BoxDecoration(color: context.blueColor),
+        padding: const EdgeInsets.only(left: 24),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                color: context.whiteColor,
+                fontWeight: FontWeight.bold,
+                fontSize: 36,
               ),
-              Text(
-                subtitle,
-                style: TextStyle(
-                  color: context.whiteColor,
-                  fontWeight: FontWeight.normal,
-                  fontSize: 28,
-                ),
+            ),
+            Text(
+              subtitle,
+              style: TextStyle(
+                color: context.whiteColor,
+                fontWeight: FontWeight.normal,
+                fontSize: 28,
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
   }
 
   @override
-  Size get preferredSize => const Size.fromHeight(156);
+  Size get preferredSize => const Size.fromHeight(196);
 }
 
 class _LoginClipPath extends CustomClipper<Path> {
