bc49ea4d1ae49008504b13fd3921b2f009778c09
savioluis <savioluislobo@gmail.com>
2025-04-11 15:28:45 -0300

    feat: add filtering support to item request

--- lib/src/modules/home/presentation/home_state.dart
+++ lib/src/modules/home/presentation/home_state.dart
@@ -11,10 +11,24 @@ class HomeSuccessState extends HomeState {
   HomeSuccessState({
     required this.user,
     required this.items,
+    this.isLoadingMoreItems = false,
   });
-  
+
   final UserModel user;
   final List<ItemModel> items;
+  final bool isLoadingMoreItems;
+
+  HomeSuccessState copyWith({
+    UserModel? user,
+    List<ItemModel>? items,
+    bool? isLoadingMoreItems,
+  }) {
+    return HomeSuccessState(
+      user: user ?? this.user,
+      items: items ?? this.items,
+      isLoadingMoreItems: isLoadingMoreItems ?? this.isLoadingMoreItems,
+    );
+  }
 }
 
 class HomeFailureState extends HomeState {
