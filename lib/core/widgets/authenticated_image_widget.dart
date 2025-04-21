a67f80fc34d520d383f2ccb81482cc13cae3733d
savioluis <savioluislobo@gmail.com>
2025-04-21 17:58:02 -0300

    feat: add authenticated image widget

---
+++ lib/core/widgets/authenticated_image_widget.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:kondus/core/repositories/i_token_repository.dart';
+import 'package:kondus/core/theme/app_theme.dart';
+
+class AuthenticatedImage extends StatelessWidget {
+  const AuthenticatedImage({
+    required this.imagePath,
+    this.size = 64,
+    this.imageFit = BoxFit.cover,
+    super.key,
+  });
+
+  final String imagePath;
+  final double size;
+  final BoxFit? imageFit;
+
+  String _getImageUrl(String imagePath) {
+    const baseUrl = 'http://0.0.0.0:8080';
+    return '$baseUrl/items/images/$imagePath';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final tokenRepository = GetIt.instance<ITokenRepository>();
+
+    return FutureBuilder<String?>(
+      future: tokenRepository.getAccessToken(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const SizedBox(
+            width: 64,
+            height: 64,
+            child: Center(child: CircularProgressIndicator(strokeWidth: 2)),
+          );
+        }
+
+        final token = snapshot.data;
+
+        return Image.network(
+          _getImageUrl(imagePath),
+          width: size,
+          height: size,
+          fit: imageFit,
+          headers: {
+            'Authorization': 'Bearer $token',
+          },
+          errorBuilder: (context, error, stackTrace) {
+            return Container(
+              width: size,
+              height: size,
+              decoration: BoxDecoration(
+                color: context.lightGreyColor.withOpacity(0.2),
+              ),
+              child: Icon(
+                Icons.hide_image_outlined,
+                color: context.onSurfaceColor.withOpacity(0.2),
+                size: 0.5625 * size,
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
