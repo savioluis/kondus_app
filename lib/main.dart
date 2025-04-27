64fc633c16e11279925ed97bc98867f9b16f79f2
savioluis <savioluislobo@gmail.com>
2025-04-27 17:34:09 -0300

    chore: initial firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:kondus/app/app.dart';
 import 'package:kondus/app/injections.dart';
+import 'package:kondus/firebase_options.dart';
 
 void main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await initInjections();
   runApp(const KondusApp());
 }
