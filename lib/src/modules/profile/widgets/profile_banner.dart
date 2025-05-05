e3e96ef4d93ca0aedf89193dab49d684e4423f72
savioluis <savioluislobo@gmail.com>
2025-05-05 05:15:24 -0300

    refactor: fix profile overflow when texts are too big

--- lib/src/modules/profile/widgets/profile_banner.dart
+++ lib/src/modules/profile/widgets/profile_banner.dart
@@ -9,76 +9,83 @@ class ProfileBanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 48,
-              backgroundColor: context.lightGreyColor.withOpacity(0.18),
-              child: Text(
-                _getInitials(model.owner),
-                style: TextStyle(
-                  color: context.blueColor,
-                  fontSize: 35,
+    final double nameSize = model.owner.length > 10 ? 30 : 36;
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 48,
+                backgroundColor: context.lightGreyColor.withOpacity(0.18),
+                child: Text(
+                  _getInitials(model.owner),
+                  style: TextStyle(
+                    color: context.blueColor,
+                    fontSize: 35,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(width: 12),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  model.owner,
-                  style: context.bodyLarge!.copyWith(fontSize: 36),
-                ),
-                Text(
-                  'Morador(a)',
-                  style: context.bodyMedium!.copyWith(fontSize: 18),
-                ),
-              ],
-            ),
-          ],
-        ),
-        const SizedBox(height: 48),
-        Row(
-          children: [
-            Icon(
-              HugeIcons.strokeRoundedLocation04,
-              size: 18,
-              color: context.blueColor,
-            ),
-            const SizedBox(width: 8),
-            Text(
-              model.local,
-              style: context.labelSmall!.copyWith(
-                fontSize: 15,
-              ),
-            ),
-          ],
-        ),
-        const SizedBox(height: 12),
-        Row(
-          children: [
-            Icon(
-              HugeIcons.strokeRoundedPinLocation03,
-              size: 18,
-              color: context.blueColor,
-            ),
-            const SizedBox(width: 8),
-            Flexible(
-              child: Text(
-                model.house,
-                style: context.labelSmall!.copyWith(
-                  fontSize: 15,
+              const SizedBox(width: 12),
+              Flexible(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      model.owner,
+                      style: context.bodyLarge!.copyWith(fontSize: nameSize),
+                    ),
+                    Text(
+                      'Morador(a)',
+                      style: context.bodyMedium!.copyWith(fontSize: 18),
+                    ),
+                  ],
                 ),
               ),
-            ),
-          ],
-        )
-      ],
+            ],
+          ),
+          const SizedBox(height: 48),
+          Row(
+            children: [
+              Icon(
+                HugeIcons.strokeRoundedLocation04,
+                size: 18,
+                color: context.blueColor,
+              ),
+              const SizedBox(width: 8),
+              Flexible(
+                child: Text(
+                  model.local,
+                  style: context.labelSmall!.copyWith(
+                    fontSize: 15,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 12),
+          Row(
+            children: [
+              Icon(
+                HugeIcons.strokeRoundedPinLocation03,
+                size: 18,
+                color: context.blueColor,
+              ),
+              const SizedBox(width: 8),
+              Flexible(
+                child: Text(
+                  model.house,
+                  style: context.labelSmall!.copyWith(
+                    fontSize: 15,
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
