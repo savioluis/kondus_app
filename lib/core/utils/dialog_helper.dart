f3cf0921127b1be10fc2d335d6b2da4494e8c49d
savioluis <savioluislobo@gmail.com>
2025-06-09 22:30:39 -0300

    refactor: enchance dialog style

--- lib/core/utils/dialog_helper.dart
+++ lib/core/utils/dialog_helper.dart
@@ -16,65 +16,78 @@ class DialogHelper {
       barrierDismissible: true,
       builder: (context) => Dialog(
         backgroundColor: context.surfaceColor,
-        insetPadding: const EdgeInsets.symmetric(horizontal: 12),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+        insetPadding: const EdgeInsets.symmetric(horizontal: 16),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
               Text(
                 title,
-                style: context.titleMedium!.copyWith(fontSize: 36),
-              ),
-              const SizedBox(height: 24),
-              Text(
-                message,
-                style: context.labelSmall!.copyWith(fontSize: 24),
+                style: context.titleLarge?.copyWith(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28,
+                  color: context.onSurfaceColor,
+                ),
                 textAlign: TextAlign.center,
               ),
-              const SizedBox(height: 48),
+              const SizedBox(height: 20),
+              Text(
+                message,
+                style: context.bodyLarge!.copyWith(
+                  fontSize: 18,
+                  color: context.onSurfaceColor,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 32),
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   Expanded(
-                    child: ElevatedButton(
+                    child: OutlinedButton(
                       onPressed: onCancel,
-                      style: ElevatedButton.styleFrom(
+                      style: OutlinedButton.styleFrom(
                         shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8)),
-                        backgroundColor: context.surfaceColor,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        side: BorderSide(color: context.primaryColor),
+                        minimumSize: const Size.fromHeight(48),
+                        padding: EdgeInsets.zero,
                       ),
                       child: Text(
                         cancelLabel,
                         style: TextStyle(
                           color: context.onSurfaceColor,
-                          fontSize: 12,
+                          fontSize: 14,
                         ),
                       ),
                     ),
                   ),
-                  const SizedBox(width: 24),
+                  const SizedBox(width: 16),
                   Expanded(
                     child: ElevatedButton(
                       onPressed: onConfirm,
                       style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8)),
                         backgroundColor: context.errorColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        minimumSize: const Size.fromHeight(48),
+                        padding: EdgeInsets.zero,
                       ),
                       child: Text(
                         confirmLabel,
                         style: TextStyle(
                           color: context.onErrorColor,
-                          fontSize: 12,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
                   ),
                 ],
               ),
-              const SizedBox(height: 24),
             ],
           ),
         ),
